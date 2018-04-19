466f935381f433162128486107f4d65612cb0dea
E-Adolfo-Q <elagosquintanilla@gmail.com>
2018-04-18 23:31:12 -0300

    nuevos cambios archivo routes

--- routes/index.js
+++ routes/index.js
@@ -7,9 +7,9 @@ const api = express.Router()
 
 api.get('/product',productCtrl.getProducts)
 api.get('/product/:productId', productCtrl.getProduct)
-api.post('/product',auth, productCtrl.saveProduct)
-api.put('/product/:productId', auth, productCtrl.updateProduct)
-api.delete('/product/:productId',auth, productCtrl.deleteProduct)
+api.post('/product',productCtrl.saveProduct)
+api.put('/product/:productId', productCtrl.updateProduct)
+api.delete('/product/:productId', productCtrl.deleteProduct)
 api.post('/signup',userCtrl.signUp)
 api.post('/signin',userCtrl.signIn)
 api.get('/private', auth, (req,res) => {
